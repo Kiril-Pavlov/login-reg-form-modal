3250abdf9a45781d9729588e2f58f56febe6158c
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-07 21:22:52 +0100

    login popup visibility done

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
   const [isRegisterOpen, setIsRegisterOpen] = useState(false)
   return (
     <div className="flex flex-col gap-4 w-full h-screen items-center justify-center">
-      <button className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300">
+      <button onClick={()=>setIsLoginOpen(!isLoginOpen)}
+        className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300"
+        >
         Login
       </button>
       <button className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300">
         Register
       </button>
-      <LoginModal/>
+      <LoginModal isOpen={isLoginOpen} setIsOpen={setIsLoginOpen}/>
       <RegisterModal/>
     </div>
   );
--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 import loginImg from "../assets/1.jfif"
 
-const LoginModal = () => {
+import {GrClose} from "react-icons/gr"
+
+const LoginModal = ({isOpen,setIsOpen}) => {
   return (
-    <div className='fixed top-0 left-0 w-full h-screen z-10 flex'>
+    <div className={isOpen===true?'flex fixed top-0 left-0 w-full h-screen z-10':"hidden"}>
         <div className='flex w-1/2 bg-red-500 h-screen'>
             <img src={loginImg} alt="Login" className='h-full object-cover'/>
         </div>
         <div className='flex items-center justify-center w-1/2 bg-white'>
             LOGIN
-            <button className='absolute top-4 right-4'>X</button>
+            <button className='absolute top-4 right-4' onClick={()=>setIsOpen(!isOpen)}>
+                <GrClose size={25}/>
+            </button>
         </div>
     </div>
   )
