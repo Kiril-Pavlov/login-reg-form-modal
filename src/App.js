36fad4a54afbe61eebb26f1d71cf835d71770512
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-07 19:57:31 +0100

    added states if login or register modal is opened

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function App() {
+  const [isLoginOpen,setIsLoginOpen] = useState(false)
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false)
   return (
     <div className="flex flex-col gap-4 w-full h-screen items-center justify-center">
       <button className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300">
