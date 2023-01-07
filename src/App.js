890e34613e445abe60459d859c931e4e69f8cc10
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-07 19:53:46 +0100

    added login and register button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
-
 function App() {
   return (
-    <div className="flex items-center justify-center">
-        working...
+    <div className="flex flex-col gap-4 w-full h-screen items-center justify-center">
+      <button className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300">
+        Login
+      </button>
+      <button className="border-2 bg-stone-50 hover:bg-stone-300 py-2 px-4 font-bold transition-all duration-300">
+        Register
+      </button>
     </div>
   );
 }
