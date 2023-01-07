936f94c3732d91db3670fd718e9e8b79324be988
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-07 23:25:43 +0100

    login modal elements and positioning done

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import loginImg from "../assets/1.jfif"
+import logo from "../assets/logo.png"
 
 import {GrClose} from "react-icons/gr"
 
@@ -10,9 +11,20 @@ const LoginModal = ({isOpen,setIsOpen}) => {
         <div className='hidden md:flex w-1/2 h-screen'>
             <img src={loginImg} alt="Login" className='h-full object-cover'/>
         </div>
-        <div className='flex items-center justify-center w-full md:w-1/2 bg-white'>
-            <div>
-                LOGIN
+        <div className='flex items-center justify-center w-full md:w-1/2 bg-sky-50'>
+            <div className='flex flex-col gap-3 w-80'>
+                <img src={logo} alt="logo" className='w-wull mx-auto drop-shadow-2xl'/>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Username or Mail</label>
+                    <input type="text"  className='bg-sky-50 border-b-2'/>
+                </div>
+                <div  className='flex flex-col'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" className='bg-sky-50 border-b-2'/>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <button className='bg-red-800 text-sky-50 font-bold text-2xl py-1 px-8 rounded-full'>Login</button>
+                </div>
             </div>
             <button className='absolute top-4 right-4' onClick={()=>setIsOpen(!isOpen)}>
                 <GrClose size={25}/>
