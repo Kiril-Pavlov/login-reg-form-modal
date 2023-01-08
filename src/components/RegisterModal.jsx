158c84b27861a5d0b4f780f0c3f00b9986da3528
Kiril-Pavlov <Kire_P@yahoo.com>
2023-01-08 18:30:57 +0100

    added states for register form

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -1,45 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import loginImg from "../assets/1.jfif"
-import logo from "../assets/logo.png"
+import loginImg from "../assets/1.jfif";
+import logo from "../assets/logo.png";
 
-import {GrClose} from "react-icons/gr"
+import { GrClose } from "react-icons/gr";
 
-const RegisterModal = ({isOpen,setIsOpen}) => {
+const RegisterModal = ({ isOpen, setIsOpen }) => {
+  //   let emailREGEX = `^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`;
+
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
+  console.log(email, username, password,repeatPassword);
+
   return (
-        <div className={isOpen===true?'flex fixed top-0 left-0 w-full h-screen z-10':"hidden"}>
-            <div className='hidden md:flex w-1/2 h-screen'>
-                <img src={loginImg} alt="Register" className='h-full object-cover'/>
-            </div>
-            <div className='flex items-center justify-center w-full md:w-1/2 bg-sky-50'>
-            <div className='flex flex-col gap-3 w-80'>
-                <img src={logo} alt="logo" className='w-wull mx-auto drop-shadow-2xl mb-3'/>
-                <div className='flex flex-col'>
-                    <label htmlFor="" className='text-red-800 font-bold'>Mail</label>
-                    <input type="text"  className='bg-sky-50 border-b-2'/>
-                </div>
-                <div className='flex flex-col'>
-                    <label htmlFor="" className='text-red-800 font-bold'>Username</label>
-                    <input type="text"  className='bg-sky-50 border-b-2'/>
-                </div>
-                <div  className='flex flex-col'>
-                    <label htmlFor="" className='text-red-800 font-bold'>Password</label>
-                    <input type="password" className='bg-sky-50 border-b-2'/>
-                </div>
-                <div  className='flex flex-col'>
-                    <label htmlFor="" className='text-red-800 font-bold'>Repeat password</label>
-                    <input type="password" className='bg-sky-50 border-b-2'/>
-                </div>
-                <div className='flex items-center justify-center'>
-                    <button className='bg-red-800 text-sky-50 font-bold text-2xl py-1 px-8 rounded-full mt-3'>Register</button>
-                </div>
-            </div>
-                <button className='absolute top-4 right-4' onClick={()=>setIsOpen(!isOpen)}>
-                    <GrClose size={25}/>
-                </button>
-            </div>
+    <div
+      className={
+        isOpen === true
+          ? "flex fixed top-0 left-0 w-full h-screen z-10"
+          : "hidden"
+      }
+    >
+      <div className="hidden md:flex w-1/2 h-screen">
+        <img src={loginImg} alt="Register" className="h-full object-cover" />
+      </div>
+      <div className="flex items-center justify-center w-full md:w-1/2 bg-sky-50">
+        <div className="flex flex-col gap-3 w-80">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-wull mx-auto drop-shadow-2xl mb-3"
+          />
+          <div className="flex flex-col">
+            <label htmlFor="" className="text-red-800 font-bold">
+              Mail
+            </label>
+            <input
+              type="email"
+              className="bg-sky-50 border-b-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="" className="text-red-800 font-bold">
+              Username
+            </label>
+            <input
+              type="text"
+              className="bg-sky-50 border-b-2"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="" className="text-red-800 font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              className="bg-sky-50 border-b-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="" className="text-red-800 font-bold">
+              Repeat password
+            </label>
+            <input 
+                type="password" 
+                className="bg-sky-50 border-b-2" 
+                value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+              />
+          </div>
+          <div className="flex items-center justify-center">
+            <button className="bg-red-800 text-sky-50 font-bold text-2xl py-1 px-8 rounded-full mt-3">
+              Register
+            </button>
+          </div>
         </div>
-  )
-}
+        <button
+          className="absolute top-4 right-4"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <GrClose size={25} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterModal
+export default RegisterModal;
